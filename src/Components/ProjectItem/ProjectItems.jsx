a68824e49e8f6650cfee4d3ac2309f9a82c4efba
Joel Munoz <j.munoz1@yahoo.com>
2025-02-23 20:38:48 -0800

    project edits

--- src/Components/ProjectItem/ProjectItems.jsx
+++ src/Components/ProjectItem/ProjectItems.jsx
@@ -1,8 +1,10 @@
 import { React, useState } from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-import { Card, Image, Offcanvas } from "react-bootstrap";
+import { Card, Container, Image, Offcanvas } from "react-bootstrap";
 import { BiWorld } from "react-icons/bi";
+import { FaLink } from "react-icons/fa6";
+
 import "./projectItem.scss";
 
 function customMapWithBreak(arr, callback, stopIteration) {
@@ -29,24 +31,18 @@ function ProjectItems({ title, img, desc, link, linkName, tool }) {
     <motion.div
       className="card-Item"
       whileHover={{
-        scale: 1.1,
+        scale: 1.05,
         boxShadowshadow: "1px 6px 20px -4px rgba(255,255,255,0.75)",
       }}
     >
+      {/* card code */}
       <Card onClick={handleShow} key={title}>
         <Card.Img variant="top" src={img} />
         <Card.Body>
           <h3>{title}</h3>
-          {/* <div className="tools-Title">Tools used:</div> */}
-          {/* tools listed here */}
-          {/* <div className="tools">
-            {mappedArray.map((item, index) => (
-              <div className="tools-Item" key={index}>{item}</div>
-            ))}
-            ...
-          </div> */}
         </Card.Body>
       </Card>
+      {/* off canvas code */}
       <Offcanvas show={show} onHide={handleClose} placement="end">
         <Offcanvas.Header closeButton>
           <h3>{title}</h3>
@@ -55,7 +51,10 @@ function ProjectItems({ title, img, desc, link, linkName, tool }) {
           <Image src={img} />
         </div>
         <Offcanvas.Body>
-          {desc}
+          <div>
+            <h6>About</h6>
+            {desc}
+          </div>
           <div>
             <h6>Technologies:</h6>
             <div className="tools">
@@ -66,10 +65,7 @@ function ProjectItems({ title, img, desc, link, linkName, tool }) {
               ))}
             </div>
           </div>
-          <div>
-            <h6>Use Case:</h6>
-            <p></p>
-          </div>
+
           <div>
             <div className="card-align">
               <BiWorld /> <h6>Link:</h6>{" "}
@@ -79,6 +75,14 @@ function ProjectItems({ title, img, desc, link, linkName, tool }) {
             </Link>
           </div>
         </Offcanvas.Body>
+        {/* off canvas bottom link positioned */}
+        <div className="off-canvas-bottom-link">
+          <h6>
+            <Link to={link} target="_blank">
+              Project link <FaLink />
+            </Link>
+          </h6>
+        </div>
       </Offcanvas>
     </motion.div>
   );
